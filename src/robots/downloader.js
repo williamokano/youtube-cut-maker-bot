cefd55fd83b2d096df9e656fac55846e62252cd4
William Johnson dos Santos Okano <williamokano@gmail.com>
2020-12-22 02:12:07 -0300

    Initial skeleton for youtube downloder bot

--- src/robots/downloader.js
+++ src/robots/downloader.js
@@ -1,6 +1,19 @@
+const fs = require('fs')
+const youtubedl = require('youtube-dl')
+
 module.exports = async config => {
-    console.log('> Download starting...')
+    console.log(`> Download starting from ${config.url}`)
+    const video = youtubedl(config.url)
+    youtubedl.exec(config.url, ['-f', 'bestvideo[ext=mp4]+bestaudio[ext=m4a]'], {},
+        function exec(err, output) {
+            'use strict'
+            if (err) {
+                throw err
+            }
+            console.log(output.join('\n'))
+        })
     config['downloaded'] = true
+
     console.log('> Download completed')
     return config
 }
